4ebf025ecdb61d06d2d3101ad92cc287a0cd40aa
miikamenk <miikarito@gmail.com>
2025-10-28 13:49:14 +0200

    tasks 2 and 3

---
+++ todo/server/init.sql
@@ -0,0 +1,16 @@
+create table task (
+    id serial primary key,
+    description carchar(255) not null
+)
+
+insert into task (description) values
+('Complete the project documentation'),
+('Review the code changes'),
+('Prepare for the team meeting'),
+('Update the project timeline'),
+('Test the new features'),
+('Fix the reported bugs'),
+('Deploy the application to production'),
+('Condu7ct a code review with peers');
+
+
