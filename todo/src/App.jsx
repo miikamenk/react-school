c42860eb9add91450411acddc63ded73328a7ede
miikamenk <miikarito@gmail.com>
2025-10-28 14:03:35 +0200

    task 4
    some stuff was missing from the part4.pdf, but it was quite easy to add

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,22 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
+import Row from "./components/Row";
+
+const url = "http://localhost:3001";
 
 function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        alert(error.response.data ? error.response.data.message : error);
+      });
+  }, []);
+
   const addTask = () => {
-    setTasks([...tasks, task]);
-    setTask("");
+    const newTask = { description: task };
+    axios
+      .post(url + "/create", { task: newTask })
+      .then((response) => {
+        setTasks([...tasks, response.data]);
+        setTask("");
+      })
+      .catch((error) => {
+        alert(error.response ? error.response.data.error.message : error);
+      });
   };
 
-  // feedback to the teachers: problem with this function is there are two or more tasks with the same string, it will delete them all
-  // a good fix for this would be to assign a unique id when creating a task and using that to delete the task
   const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter((item) => item !== deleted);
-    setTasks(withoutRemoved);
+    axios
+      .delete(url + "/delete/" + deleted)
+      .then((response) => {
+        setTasks(tasks.filter((item) => item.id !== deleted));
+      })
+      .catch((error) => {
+        alert(error.response ? error.response.data.error.message : error);
+      });
   };
 
   return (
@@ -38,12 +65,7 @@ function App() {
       </form>
       <ul>
         {tasks.map((item) => (
-          <li>
-            {item}
-            <button className="delete-button" onClick={() => deleteTask(item)}>
-              delete
-            </button>
-          </li>
+          <Row item={item} key={item.id} deleteTask={deleteTask} />
         ))}
       </ul>
     </div>
